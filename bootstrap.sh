25859e77ba23ae1a3b672601c25badd5ec852b8e
Caolan Cochrane <caolan.cochrane2@mail.dcu.ie>
2025-01-25 15:49:07 +0000

    added tlp

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,9 @@ sudo dnf install -y $(cat ~/.fedora-configs/dnf-packages.txt)
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 sudo chsh -s $(which zsh)
 
+sudo systemctl start tlp
+sudo systemctl enable tlp
+
 exec zsh
 
 zinit self-update
